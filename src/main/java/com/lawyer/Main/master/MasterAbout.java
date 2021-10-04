6f04bd9dbf54d257855e01ceb4a91c411a68c9e8
luoyang_glb <2810325944_glb@qq.com>
2021-10-04 19:57:00 +1400

    by master

---
+++ src/main/java/com/lawyer/Main/master/MasterAbout.java
@@ -0,0 +1,9 @@
+package com.lawyer.Main.master;
+
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+
+public class MasterAbout {
+    public static void main(String[] args) {
+        System.out.println("老子是master");
+    }
+}
