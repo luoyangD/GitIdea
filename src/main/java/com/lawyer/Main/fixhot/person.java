eb3cf827d3ae636effe42c416700e4b2434cea85
luoyang_glb <2810325944_glb@qq.com>
2021-10-04 19:46:46 +1400

    hot fix

---
+++ src/main/java/com/lawyer/Main/fixhot/person.java
@@ -0,0 +1,25 @@
+package com.lawyer.Main.fixhot;
+
+public class person {
+    private int num;
+    private String name;
+
+    public person() {
+    }
+
+    public int getNum() {
+        return num;
+    }
+
+    public void setNum(int num) {
+        this.num = num;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
